3576b1c010828674e44538d90c9299eaa62f7bbe
bewakes <bewakepandey@gmail.com>
2021-09-29 15:06:55 +0545

    Minor changes, some renames and code styling

--- src/BSForm.tsx
+++ src/BSForm.tsx
@@ -3,7 +3,7 @@ import { Row, Col, Form as BForm, Input, Label, FormGroup, Button } from 'reacts
 
 import { Layout, UseForm, Schema, SchemaSpec, LayoutElement, ProcessedLayoutRow, Action } from './types';
 import { Errors, nestifyValues } from './useForm';
-import { parseFileName, filterObject } from './utils';
+import { parseFileName } from './utils';
 
 type FormProps<T> = {
     form: UseForm<T>;
@@ -14,7 +14,7 @@ type FormProps<T> = {
     actions?: Action<T>[];
     disabled?: boolean;
     actionsTop?: boolean; 
-    alertType?: "circular" | "linear";
+    alertType?: 'circular' | 'linear';
     showWaitAlert?: boolean;
     setShowWaitAlert?: (x: boolean) => any;
     waitAlertMessage?: string;
@@ -37,50 +37,48 @@ function WrappedInput<T> (props: WrappedInputProps<T>) {
     let renderValue = renderer(value);
 
     const removeFileSelection = (index: number, selectionType: "currSelections"|"prevSelections") => {
-            const updatedFileValues = formValues[name + "." + selectionType].filter((_: any, i:number) => i!=index);
-            const updatedFormValues = { ...formValues, [name + "." + selectionType]: updatedFileValues };
-            setFormValues(updatedFormValues);
+        const updatedFileValues = formValues[name + "." + selectionType].filter((_: any, i:number) => i!=index);
+        const updatedFormValues = { ...formValues, [name + "." + selectionType]: updatedFileValues };
+        setFormValues(updatedFormValues);
 
-            const err = schema.validation? schema.validation(nestifyValues(updatedFormValues)[name], updatedFormValues): undefined;
+        const err = schema.validation ? schema.validation(nestifyValues(updatedFormValues)[name], updatedFormValues): undefined;
 
-            if(typeof(err) === 'string'){
-                setFormErrors({...formErrors,
-                        [name]: err
-                    })
+        if (typeof err === 'string') {
+            setFormErrors({ ...formErrors, [name]: err });
+        } else {
+            if (formErrors && formErrors[name]) {
+                const { [name]: _, ...updatedFormErrors } = formErrors;
+                setFormErrors(updatedFormErrors);
             }
-            else{
-                if(formErrors && formErrors[name]){
-                    const {[name]:_, ...updatedFormErrors} = formErrors;
-                    setFormErrors(updatedFormErrors);
-                }
-            }
+        }
     }
-    if((schema.displayCondition && !schema.displayCondition(formValues)) || 
+    if ((schema.displayCondition && !schema.displayCondition(formValues)) ||
         (schema.hideCondition && schema.hideCondition(formValues))) {
         return null;
     }
-    if (schema.type === "select") {
+    if (schema.type === 'select') {
         return (
             <React.Fragment>
                 <Label>
-                    {schema.label}{ schema.required && <span className="input-error">*</span> }
-                    <small className="input-error">{formErrors && formErrors[name]}</small>
+                    {schema.label}
+                    {schema.required && <span className="input-error">*</span>}
+                    <small className="input-error">
+                        {formErrors && formErrors[name]}
+                    </small>
                 </Label>
                 <Input
                     name={name as string}
                     value={renderValue}
                     type="select"
                     invalid={!!(formErrors && formErrors[name])}
-                    onChange={ onChange }
+                    onChange={onChange}
                     {...other}
                 >
-                {
-                    (schema.options || []).map(x => (
+                    {(schema.options || []).map((x) => (
                         <option key={x.value} value={x.value}>
                             {x.label}
                         </option>
-                    ))
-                }
+                    ))}
                 </Input>
             </React.Fragment>
         );
@@ -93,104 +91,147 @@ function WrappedInput<T> (props: WrappedInputProps<T>) {
                         type="checkbox"
                         invalid={!!(formErrors && formErrors[name])}
                         checked={renderValue}
-                        onChange={ onChange }
+                        onChange={onChange}
                         {...other}
                     />{' '}
-                    {schema.label}{ schema.required && <span className="input-error">*</span> }
+                    {schema.label}
+                    {schema.required && <span className="input-error">*</span>}
                 </Label>
             </FormGroup>
         );
     }
     if (schema.type === 'label') {
-        return <h5 style={{marginBottom: "-8px", marginTop: "8px"}}>{schema.label}</h5>;
-    }
-    if (schema.type === 'file'){
-        const mParseFileName = schema.parseFileName? schema.parseFileName : parseFileName;
-        return  ( 
-        <FormGroup>
-            <Label style={{ display: "block"}}>
-                { schema.label }
-                <small className="input-error">{formErrors && formErrors[name]}</small>
-            </Label>
-            <Input 
-                type="file"
-                name={name as string}  
-                multiple={true}
-                accept={ schema.allowedFileExtensions? schema.allowedFileExtensions : "*" } 
-                onChange={ onChange }
-                { ...other } 
-                style={{ display: "none" }}
-                id="uploadFile"
-            />
-            <button type="button" className="browse-btn" onClick={ () => document.getElementById("uploadFile")?.click() }>
-                Browse...
-            </button>
-            {
-                formValues[name+'.currSelections'] && 
-                    Object.entries(formValues[name+'.currSelections']).map(
-                        ([key, file]) => 
-                            <div key={ key }>
-                                <span>{ (file as File).name }</span>
-                                <span className="remove-file" onClick={ () => removeFileSelection(+key, "currSelections")}>X</span>
-                            </div>
-                )
-            }
-            {
-                formValues[name+'.prevSelections'] && formValues[name+'.prevSelections'].length !== 0 &&
-                    <div>
-                        <hr/>
-                        <b>Previous selected files:</b>
-                        {
-                            formValues[name+'.prevSelections'].map(mParseFileName).map(
-                                (filename:string, index:number) => 
-                                    <div key={ index }>
-                                        <span>{ filename }</span>
-                                        <span className="remove-file" onClick={ () => removeFileSelection(+index, "prevSelections")}>X</span>
-                                    </div>
-                            )
-                        }
-                    </div>
-            }
-        </FormGroup>
+        return (
+            <h5 style={{ marginBottom: '-8px', marginTop: '8px' }}>
+                {schema.label}
+            </h5>
         );
     }
-    if(schema.type === "date" && renderValue && renderValue.length > 10){
+    if (schema.type === 'file') {
+        const mParseFileName = schema.parseFileName
+            ? schema.parseFileName
+            : parseFileName;
+        return (
+            <FormGroup>
+                <Label style={{ display: 'block' }}>
+                    {schema.label}
+                    <small className="input-error">
+                        {formErrors && formErrors[name]}
+                    </small>
+                </Label>
+                <Input 
+                    type="file"
+                    name={name as string}
+                    multiple
+                    accept={schema.allowedFileExtensions || '*'}
+                    onChange={onChange}
+                    {...other}
+                    style={{ display: 'none' }}
+                    id="uploadFile"
+                />
+                <button
+                    type="button"
+                    className="browse-btn"
+                    onClick={() =>
+                        document.getElementById('uploadFile')?.click()
+                    }
+                >
+                    Browse...
+                </button>
+                {formValues[name + '.currSelections'] &&
+                    Object.entries(formValues[name + '.currSelections']).map(
+                        ([key, file]) => (
+                            <div key={key}>
+                                <span>{(file as File).name}</span>
+                                <span
+                                    className="remove-file"
+                                    onClick={() => removeFileSelection(+key, 'currSelections')}
+                                >
+                                    X
+                                </span>
+                            </div>
+                        )
+                )}
+                {formValues[name + '.prevSelections'] &&
+                    formValues[name + '.prevSelections'].length !== 0 && (
+                        <div>
+                            <hr/>
+                            <b>Previous selected files:</b>
+                            {formValues[name + '.prevSelections']
+                                .map(mParseFileName)
+                                .map((filename: string, index: number) => ( 
+                                    <div key={index}>
+                                        <span>{filename}</span>
+                                        <span
+                                            className="remove-file"
+                                            onClick={() => removeFileSelection(+index, "prevSelections")}
+                                        >
+                                            X
+                                        </span>
+                                    </div>
+                                ))}
+                        </div>
+                    )}
+            </FormGroup>
+        );
+    }
+    if (schema.type === 'date' && renderValue && renderValue.length > 10) {
         renderValue = renderValue.substring(0, 10);
     }
     return (
         <React.Fragment>
             <Label>
-                {schema.label}{ schema.required && <span className="input-error">*</span> }
-                <small className="input-error">{formErrors && formErrors[name]}</small>
+                {schema.label}
+                {schema.required && <span className="input-error">*</span>}
+                <small className="input-error">
+                    {formErrors && formErrors[name]}
+                </small>
             </Label>
-        <Input
-            name={name as string}
-            value={renderValue}
-            type={schema.type}
-            placeholder={schema.placeholder}
-            invalid={!!(formErrors && formErrors[name])}
-            onChange={ onChange }
-            {...other}
-        />
+            <Input
+                name={name as string}
+                value={renderValue}
+                type={schema.type}
+                placeholder={schema.placeholder}
+                invalid={!!(formErrors && formErrors[name])}
+                onChange={onChange}
+                {...other}
+            />
         </React.Fragment>
     );
 };
 
 const Form: <T>(_: FormProps<T>) => React.ReactElement<FormProps<T>> = (props) => {
     const {
-        submitCallback, layout, schema, form,
-        disabled, actions,
-        actionName, actionsTop 
+        submitCallback,
+        layout,
+        schema,
+        form,
+        disabled,
+        actions,
+        actionName,
+        actionsTop
     } = props;
 
-    const { formValues, formErrors, onChange, onSubmit, setFormValues, setFormErrors, validateAndSetErrors } = form;
+    const {
+        formValues,
+        formErrors,
+        onChange,
+        onSubmit,
+        setFormValues,
+        setFormErrors,
+        validateAndSetErrors
+    } = form;
     // TODO: use formErrors
     type FormType = typeof formValues;
 
-    const processRow = <T extends unknown>(row: LayoutElement<T>[], fullWidth: number): ProcessedLayoutRow<T> => {
-        if(typeof row == "string") return [[row, fullWidth]];
+    const processRow = <T extends unknown>(
+        row: LayoutElement<T>[], fullWidth: number
+    ): ProcessedLayoutRow<T> => {
+        if (typeof row === 'string') return [[row, fullWidth]];
         const width = fullWidth/row.length;
-        return row.map(item => processRow(item as Array<keyof T>, width)).flat();
+        return row
+            .map((item) => processRow(item as Array<keyof T>, width))
+            .flat();
     };
     React.useEffect(() => {
         form.setSchema(schema);
@@ -199,53 +240,43 @@ const Form: <T>(_: FormProps<T>) => React.ReactElement<FormProps<T>> = (props) =
     const processedLayout = layout.map(row => processRow<FormType>(row, 12));
     const buttons = (
         <React.Fragment>
-        { actionName && <Button color="success" style={{marginRight: '3px'}}>{actionName}</Button> }
-        {
-            (actions||[]).map(action => (
+            {actionName &&
+                <Button color="success" style={{ marginRight: '3px' }}>
+                    {actionName}
+                </Button>
+            }
+            {(actions || []).map((action) => (
                 <Button
                     key={action.name}
                     type="button"
-                    color={action.color || "primary"}
+                    color={action.color || 'primary'}
                     onClick={() => {
-                            if(action && action.custom && action.custom.method && (action.custom.method === "put" || action.custom.method === "post")) {
-                                const valid = validateAndSetErrors();
-
-                                if (!valid) return ;
-
-                                if (schema) {
-                                    const filtered = filterObject(
-                                        formValues,
-                                        form.filterObjFunc
-                                    );
-                                    action.callback({ ...form.initvalues, ...nestifyValues(filtered) }, action);
-                                } else {
-                                    // eslint:disable-next-line
-                                    action.callback(nestifyValues(formValues), action); // TODO: gracefully handle nested type
-                                }
-                            }
-                            else{
-                                action.callback(formValues, action)
-                            }
+                        if (action && action.custom && action.custom.method && (action.custom.method === "put" || action.custom.method === "post")) {
+                            const valid = validateAndSetErrors();
+                            if (!valid) return;
+                            action.callback(form.getFilteredValues(), action);
+                        } else {
+                            action.callback(formValues, action)
                         }
-                    }
-                    style={{marginRight:'3px'}}
+                    }}
+                    style={{ marginRight:'3px' }}
                 >
                     {action.name}
                 </Button>
-            ))
-        }
+            ))}
         </React.Fragment>
     );
     return (
-        <BForm className="tf-form" onSubmit={onSubmit(submitCallback? submitCallback: () => {})}>
+        <BForm
+            className="tf-form"
+            onSubmit={onSubmit(submitCallback || (() => {}))}
+        >
             <fieldset disabled={!!disabled}>
             {actionsTop && <React.Fragment>{buttons}<hr/></React.Fragment>} 
-            {
-                processedLayout.map((rows, i: number) => (
-                    <Row key={i}>
-                    {
-                        rows.map(([fieldName, width]: [any, number]) => (
-                            <Col md={width} key={fieldName as string}>
+            {processedLayout.map((rows, i: number) => (
+                <Row key={i}>
+                {rows.map(([fieldName, width]: [any, number]) => (
+                        <Col md={width} key={fieldName as string}>
                             <FormGroup>
                                 <WrappedInput<FormType>
                                     schema={schema[fieldName as (keyof FormType)]}
@@ -258,14 +289,11 @@ const Form: <T>(_: FormProps<T>) => React.ReactElement<FormProps<T>> = (props) =
                                     formErrors={formErrors}
                                 />
                             </FormGroup>
-                            </Col>
-                        ))
-                    }
-                    </Row>
-                ))
-            }
+                        </Col>
+                    ))}
+                </Row>
+            ))}
             {!actionsTop && <React.Fragment><hr />{buttons}</React.Fragment>}
-          
             </fieldset>
         </BForm>
     );
